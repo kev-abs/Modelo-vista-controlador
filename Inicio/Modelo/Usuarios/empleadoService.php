4f618be0670e910a9cb0cd272fa362b07c43d3ec
user.email <manuel25142808@gmail.com>
2025-10-01 22:00:10 -0500

    Token áctivo por 24 horas

--- Inicio/Modelo/Usuarios/empleadoService.php
+++ Inicio/Modelo/Usuarios/empleadoService.php
@@ -7,7 +7,7 @@ class EmpleadoService {
         $this->apiUrl = $urlEmpleado;
     }
 
-    private $jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTc1OTI2OTA4MywiZXhwIjoxNzU5MjcyNjgzfQ.lceubp1ut5mqgxP0uW1KnoHtUTLzoM9m5GE2SgX44H4";
+    private $jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTc1OTM3MzE0NiwiZXhwIjoxNzU5NDU5NTQ2fQ.zYpTFWgsukxyEagLCKPYtMMRMpWuyOQLBgAizM88670";
 
     public function obtenerEmpleados() {
 
